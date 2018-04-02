3b8df25c14b466e0520ad8dc92f61cc49d22dacd
Jonathan Tiede <skysoldiermars@gmail.com>
2018-04-02 23:03:01 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -41,7 +41,7 @@ class Student
   end
 
   def self.first_student_in_grade_10
-    test = DB[:conn].execute("SELECT * FROM students WHERE grade = 10 LIMIT 1")[0][0]
+    DB[:conn].execute("SELECT * FROM students WHERE grade = 10 LIMIT 1")[0]
 
   end
 
